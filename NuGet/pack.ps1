d9c96696085566a1a4148cb9aea054fb07bf8669
Josh Keegan <josh@visav.co.uk>
2016-09-07 14:08:17 +0100

    Include actual nuspec & comment previous generation (so it can still be used in future if we want to start from scratch for whatever reason)

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -4,9 +4,9 @@ if($env:APPVEYOR_REPO_BRANCH -eq "release")
 	$version = [System.Reflection.Assembly]::LoadFile("$root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll").GetName().Version
 	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
 	
-	Write-Host "Generating nuspec"
-	nuget spec $root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll
-	cp $root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll.nuspec $root\NuGet\SqlServerHelpers.nuspec
+	#Write-Host "Generating nuspec"
+	#nuget spec $root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll
+	#cp $root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll.nuspec $root\NuGet\SqlServerHelpers.nuspec
 
 	Write-Host "Setting .nuspec version tag to $versionStr"
 
