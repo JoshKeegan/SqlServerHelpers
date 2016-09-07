d5ef405a8740c4a20a8878b7e7074d356c6b8f84
Josh Keegan <josh@visav.co.uk>
2016-09-07 13:49:44 +0100

    Nuget release (first attempt - not expecting to be right!)

---
+++ NuGet/pack.ps1
@@ -0,0 +1,22 @@
+if($env:APPVEYOR_REPO_BRANCH -eq "release")
+{
+	$root = (split-path -parent $MyInvocation.MyCommand.Definition) + '\..'
+	$version = [System.Reflection.Assembly]::LoadFile("$root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll").GetName().Version
+	$versionStr = "{0}.{1}.{2}.{3}" -f ($version.Major, $version.Minor, $version.Build, $version.Revision)
+	
+	Write-Host "Generating nuspec"
+	nuget spec $root\SqlServerHelpers\bin\Release\SqlServerHelpers.dll
+
+	Write-Host "Setting .nuspec version tag to $versionStr"
+
+	$content = (Get-Content $root\NuGet\SqlServerHelpers.nuspec) 
+	$content = $content -replace '\$version\$', $versionStr
+
+	$content | Out-File $root\NuGet\SqlServerHelpers.compiled.nuspec
+
+	& nuget pack $root\NuGet\SqlServerHelpers.compiled.nuspec
+}
+else
+{
+	Write-Host "Not packaging for nuget. To package & push to nuget, merge onto the release branch"
+}
