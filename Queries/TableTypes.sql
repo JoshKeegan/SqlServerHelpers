24f1e8f26f5bba46c7aef9c70deabf3aabcabdd8
Josh Keegan <josh@visav.co.uk>
2016-11-29 12:07:14 +0000

    Drop existing table types if they already exist when creating

--- Queries/TableTypes.sql
+++ Queries/TableTypes.sql
@@ -6,31 +6,43 @@
 		Josh Keegan 20/09/2016
 */
 
+IF TYPE_ID('dbo.TableType_Generic_Int') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_Int;
 CREATE TYPE dbo.TableType_Generic_Int AS TABLE
 (
 	v int NOT NULL
 );
 
+IF TYPE_ID('dbo.TableType_Generic_Int_Nullable') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_Int_Nullable;
 CREATE TYPE dbo.TableType_Generic_Int_Nullable AS TABLE
 (
 	v int NULL
 );
 
+IF TYPE_ID('dbo.TableType_Generic_BigInt') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_BigInt;
 CREATE TYPE dbo.TableType_Generic_BigInt AS TABLE
 (
 	v bigint NOT NULL
 );
 
+IF TYPE_ID('dbo.TableType_Generic_BigInt_Nullable') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_BigInt_Nullable;
 CREATE TYPE dbo.TableType_Generic_BigInt_Nullable AS TABLE
 (
 	v bigint NULL
 );
 
+IF TYPE_ID('dbo.TableType_Generic_VarChar_Nullable') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_VarChar_Nullable;
 CREATE TYPE dbo.TableType_Generic_VarChar_Nullable AS TABLE
 (
 	v varchar(max) NULL
 );
 
+IF TYPE_ID('dbo.TableType_Generic_VarChar') IS NOT NULL
+	DROP TYPE dbo.TableType_Generic_VarChar;
 CREATE TYPE dbo.TableType_Generic_VarChar AS TABLE
 (
 	v varchar(max) NOT NULL
