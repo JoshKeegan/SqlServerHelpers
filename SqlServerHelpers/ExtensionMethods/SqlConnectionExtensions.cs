e0c2e75290583de1fa60046d026c33b3533e7da4
Josh Keegan <josh@visav.co.uk>
2017-06-05 15:53:20 +0100

    Allow non-default Command Settings to be passed to GetSqlCommand()

--- SqlServerHelpers/ExtensionMethods/SqlConnectionExtensions.cs
+++ SqlServerHelpers/ExtensionMethods/SqlConnectionExtensions.cs
@@ -16,31 +16,54 @@ namespace SqlServerHelpers.ExtensionMethods
 {
     public static class SqlConnectionExtensions
     {
-        public static SqlCommand GetSqlCommand(this SqlConnection conn, SqlTransaction trans = null)
+        #region Public Methods
+
+        public static SqlCommand GetSqlCommand(this SqlConnection conn, SqlTransaction trans = null,
+            CommandSettings commandSettings = null)
         {
-            return getSqlCommand(null, conn, trans);
+            return getSqlCommand(null, conn, trans, commandSettings);
         }
 
-        public static SqlCommand GetSqlCommand(this SqlConnection conn, string txtCmd, SqlTransaction trans = null)
+        public static SqlCommand GetSqlCommand(this SqlConnection conn, string txtCmd, SqlTransaction trans = null,
+            CommandSettings commandSettings = null)
         {
-            return getSqlCommand(txtCmd, conn, trans);
+            return getSqlCommand(txtCmd, conn, trans, commandSettings);
         }
 
-        private static SqlCommand getSqlCommand(string txtCmd, SqlConnection conn, SqlTransaction trans)
+        public static SqlCommand GetSqlCommand(this SqlConnection conn, CommandSettings commandSettings)
         {
+            return GetSqlCommand(conn, null, commandSettings);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private static SqlCommand getSqlCommand(string txtCmd, SqlConnection conn, SqlTransaction trans,
+            CommandSettings commandSettings)
+        {
+            // If no command settings have been supplied, use the default ones as defined statically in Settings
+            if (commandSettings == null)
+            {
+                commandSettings = Settings.Command;
+            }
+
+            // Make the command
             SqlCommand command = new SqlCommand(txtCmd, conn, trans);
 
-            // Apply default command settings
-            if (Settings.Command != null)
+            // Apply command settings
+            if (commandSettings != null)
             {
-                if (Settings.Command.CommandTimeout != null)
+                if (commandSettings.CommandTimeout != null)
                 {
-                    command.CommandTimeout = (int) Settings.Command.CommandTimeout;
+                    command.CommandTimeout = (int) commandSettings.CommandTimeout;
                 }
                 // TODO: Support more default settings as they're added to CommandSettings
             }
 
             return command;
         }
+
+        #endregion
     }
 }
